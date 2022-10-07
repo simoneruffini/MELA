6c554c736dd25580d025993b607c970c9ce20607
Simone Ruffini <simone.ruffini@tutanota.com>
2022-10-07 22:35:40 +0200

    feat: piepeline "done" (RF missing)

--- src/00-common.core/000-DLX_PKG.vhd
+++ src/00-common.core/000-DLX_PKG.vhd
@@ -19,61 +19,85 @@
 library ieee;
   use ieee.std_logic_1164.all;
   use ieee.numeric_std.all;
+  use ieee.math_real.all;
 
 library ieee;
   use work.alu_pkg.all;
 
 package DLX_PKG is
 
-  -- DLX GLOBAL CONSTANTS
-  constant C_ARCH_BIT_DEPTH : integer := 32; -- DLX Architecture bit depth
-  constant C_INSTR_OPCODE_W : integer := 6;  -- DLX Instruction Operation Code width
-  constant C_INSTR_FIELD_W  : integer := 5;  -- DLX Instruction Generic Field width
+  -- ilog2 returs always the ceiling of the log2 of x
 
-  -- DERIVED CONSTANTS
-  constant C_ARCH_WORD_W       : integer := C_ARCH_BIT_DEPTH; -- DLX architecture word width
-  constant C_ALU_PRECISION_BIT : integer := C_ARCH_BIT_DEPTH;
-  constant C_INSTR_RS1_W       : integer := C_INSTR_FIELD_W;
-  constant C_INSTR_RS2_W       : integer := C_INSTR_FIELD_W;
-  constant C_INSTR_RS3_W       : integer := C_INSTR_FIELD_W;
+  function ilog2 (x: natural) return natural;
+
+  -- This function checks the derived constants calculated in the DLX_PKG
+  -- NB run this function in the top_level of the DLX design
+
+  procedure dlx_pkg_check_assertions;
+
+  -- DLX GLOBAL CONSTANTS ======================================================
+  -- ===========================================================================
+
+  -- DLX Architecture bit depth
+  constant C_ARCH_BIT_DEPTH : integer := 32;
+  -- DLX Instruction Operation Code width
+  constant C_INSTR_OPCODE_W : integer := 6;
+  -- DLX Register File Address Width
+  -- => DLX RF address space == 32
+  constant C_RF_ADDR_W : integer := ilog2(32);
+  -- DLX Instruction Memory Address Width
+  -- => DLX I-MEM address space == 2**C_DMEM_ADDRW
+  constant C_IMEM_ADDR_W : integer := 8;
+  -- DLX Data Memory Address Width
+  -- => DLX D-MEM address space == 2**C_DMEM_ADDRW
+  constant C_DMEM_ADDR_W : integer := 8;
+
+  -- DERIVED CONSTANTS =========================================================
+  -- ===========================================================================
+
+  -- DLX Instruction Generic Field width (5)
+  constant C_INSTR_FIELD_W : integer := C_RF_ADDR_W;
+  -- DLX architecture word width
+  constant C_ARCH_WORD_W             : integer := C_ARCH_BIT_DEPTH;
+  constant C_ALU_PRECISION_BIT       : integer := C_ARCH_BIT_DEPTH;
+  constant C_INSTR_RS1_START_POS_BIT : integer := C_ARCH_BIT_DEPTH - (C_INSTR_OPCODE_W + C_INSTR_FIELD_W);
+  constant C_INSTR_RS1_W             : integer := C_INSTR_FIELD_W;
+  constant C_INSTR_RS2_START_POS_BIT : integer := C_INSTR_RS1_START_POS_BIT - C_INSTR_FIELD_W;
+  constant C_INSTR_RS2_W             : integer := C_INSTR_FIELD_W;
+  constant C_INSTR_RS2_START_POS_BIT : integer := C_INSTR_RS2_START_POS_BIT - C_INSTR_FIELD_W;
+  constant C_INSTR_RS3_W             : integer := C_INSTR_FIELD_W;
+  -- DLX Instruction I-Type immediate field width (16)
+  constant C_INSTR_I_TYPE_IMM_W : integer := C_ARCH_BIT_DEPTH - (C_INSTR_OPCODE_W + 2 * C_INSTR_FIELD_W);
+  -- I type instructions have 2 operand fields and then the immediate field
+  constant C_INSTR_I_TYPE_IMM_START_POS_BIT : integer := C_INSTR_RS2_START_POS_BIT - C_INSTR_I_TYPE_IMM_W;
+  -- DLX Instruction J-Type immediate field width (26)
+  constant C_INSTR_J_TYPE_IMM_W : integer := C_ARCH_BIT_DEPTH - C_INSTR_OPCODE_W;
+  -- J type instruction have only one field
+  constant C_INSTR_J_TYPE_IMM_START_POS_BIT : integer := C_ARCH_BIT_DEPTH - (C_INSTR_FIELD_W + C_INSTR_J_TYPE_IMM_W);
 
   type cntrl_wrd_t is record
-    -- Fetch
-    -- Decode
-    jal      : std_logic;
-    rf_ra_en : std_logic;
-    rf_rb_en : std_logic;
-    rf_wen   : std_logic;
-    reg_we   : std_logic;
-    -- Execute
+    --======================= Fetch
+    branch_en : std_logic;     -- selects wheter the PC will take the branch instruction address instead of PC+4
+    jump_en   : std_logic;     -- selects wheter the PC will take the jump instruction address instead of PC+4
+    --======================= Decode
+    jal_en : std_logic;
+    -- rf_ra_en : std_logic;
+    -- rf_rb_en : std_logic;
+    rf_wen         : std_logic;
+    j_type_imm_sel : std_logic;
+    --======================= Execute
     r_type_sel   : std_logic;  -- selects whethere the write back address is the one obtained from an i-type INSTR or a R-TYPE
     imm_sel      : std_logic;
     pc_pls_4_sel : std_logic;
     alu_func     : alu_func_t; -- the convertion for this signal is done inside the control unit (integer to alu_func_t)
-  -- Memory
-  -- Writeback
+    --======================= Memory
+    dmem_wen : std_logic;
+    --======================= Writeback
+    rf_wb_dmem_dout_sel : std_logic;
   end record cntrl_wrd_t;
 
-  type cw_signals_type is (
-    -- Decode
-    A_RE,
-    B_RE,
-    REG_WE,
-    -- Execute
-    IMM_SEL,
-    PC_SEL,
-    ALU_OP0,
-    ALU_OP1,
-    ALU_OP2,
-    ALU_OP3,
-    J_TYPE,
-    -- Memory
-    DM_RE,
-    DM_WE,
-    -- Writeback
-    WB_MUX,
-    CW_SIZE
-  );
+  -- TODO maybe place constant in different file
+  constant C_JAL_RET_ADDR_REG : integer := 31; -- Register number in which jal instruction will save the return address
 
   ---- Position of the control word signals. Used internally to map the internal
   ---- std_logic_vector to the output signals.
@@ -108,3 +132,43 @@ package DLX_PKG is
   constant ALU_OR_CW   : std_logic_vector(ALU_CW_SIZE - 1 downto 0) := "0011"; -- ALU1=1 ALU2=1
 
 end package DLX_PKG;
+
+package body DLX_PKG is
+
+  procedure dlx_pkg_check_assertions is
+
+  begin
+
+    -- Asserts on derived constants
+    assert C_INSTR_FIELD_W > 0
+      report "The constant C_INSTR_FIELD_W is less then equal 0, check why"
+      severity failure;
+    assert C_INSTR_I_TYPE_IMM_W > 0
+      report "The constant C_INSTR_I_TYPE_IMM_W is less then equal 0, check why"
+      severity failure;
+    assert C_INSTR_J_TYPE_IMM_W > 0
+      report "The constant C_INSTR_J_TYPE_IMM_W is less then equal 0, check why"
+      severity failure;
+    assert C_INSTR_RS1_START_POS_BIT >= 0
+      report "The constant C_INSTR_RS1_START_POS_BIT is negative, check displacements"
+      severity failure;
+    assert C_INSTR_RS2_START_POS_BIT >= 0
+      report "The constant C_INSTR_RS2_START_POS_BIT is negative, check displacements"
+      severity failure;
+    assert C_INSTR_I_TYPE_IMM_START_POS_BIT >= 0
+      report "The constant C_INSTR_I_TYPE_IMM_START_POS_BIT is negative, check displacements"
+      severity failure;
+    assert C_INSTR_J_TYPE_IMM_START_POS_BIT >= 0
+      report "The constant C_INSTR_J_TYPE_IMM_START_POS_BIT is negative, check displacements"
+      severity failure;
+
+  end dlx_pkg_check_assertions;
+
+  function ilog2 (x:natural) return natural is
+  begin
+
+    return natural(ceil(log2(real(x))));
+
+  end function;
+
+end package body DLX_PKG;
--- src/a.b-datapath.vhd
+++ src/a.b-datapath.vhd
@@ -42,7 +42,7 @@ end entity DATAPATH;
 architecture BEHAVIORAL of DATAPATH is
 
   ----------------------------------------------------------- CONSTANTS 1
-  constant C_REG_INIT_VAL : integer := 0;
+  constant C_REG_INIT_VAL   : integer := 0;
 
   ----------------------------------------------------------- TYPES
 
@@ -54,49 +54,228 @@ architecture BEHAVIORAL of DATAPATH is
 
   -- Fetch Stage Signals (_f)
 
+  signal npc_sel_f          : std_logic;
+  signal npc_f              : std_logic_vector(C_ARCH_WORD_W - 1 downto 0);        -- new program counter
+  signal pc_f               : std_logic_vector(C_ARCH_WORD_W - 1 downto 0);        -- program counter
+  signal pc_pls_4_f         : std_logic_vector(C_ARCH_WORD_W - 1 downto 0);        -- program counter plus 4
+  signal instr_f            : std_logic_vector(C_ARCH_WORD_W - 1 downto 0);        -- instruction word
+
   -- Decode Stage Signals (_d)
 
+  signal pc_pls_4_d         : std_logic_vector(C_ARCH_WORD_W - 1 downto 0);        -- see in fetch stage
+  signal instr_d            : std_logic_vector(C_ARCH_WORD_W - 1 downto 0);        -- see in fetch stage
+  signal rs1_d              : std_logic_vector(C_INSTR_RS2_W - 1 downto 0);
+  signal rs2_d              : std_logic_vector(C_INSTR_RS2_W - 1 downto 0);
+  signal rs3_d              : std_logic_vector(C_INSTR_RS3_W - 1 downto 0);
+  signal imm_i_type_d       : std_logic_vector(C_INSTR_I_TYPE_IMM_W - 1 downto 0); -- immediate field of I type instructions
+  signal imm_j_type_d       : std_logic_vector(C_INSTR_J_TYPE_IMM_W - 1 downto 0); -- immediate field of J type instructions
+  signal imm_i_type_ext_d   : std_logic_vector(C_ARCH_WORD_W - 1 downto 0);        -- immediate field of I type instructions EXTENDED
+  signal imm_j_type_ext_d   : std_logic_vector(C_ARCH_WORD_W - 1 downto 0);        -- immediate field of J type instructions EXTENDED
+  signal imm_d              : std_logic_vector(C_ARCH_WORD_W - 1 downto 0);        -- immediate field going to executed stage
+  signal rf_waddr_d         : std_logic_vector(C_RF_ADDR_W - 1 downto 0);
+  signal rf_din_d           : std_logic_vector(C_ARCH_WORD_W - 1 downto 0);
+  signal rf_dout1_d         : std_logic_vector(C_ARCH_WORD_W - 1 downto 0);
+  signal rf_dout2_d         : std_logic_vector(C_ARCH_WORD_W - 1 downto 0);
+
   -- Execute Stage Signals (_e)
-  signal rs2_e            : std_logic_vector(C_INSTR_RS2_W - 1 downto 0);
-  signal rs3_e            : std_logic_vector(C_INSTR_RS3_W - 1 downto 0);
+  signal rs2_e              : std_logic_vector(C_INSTR_RS2_W - 1 downto 0);
+  signal rs3_e              : std_logic_vector(C_INSTR_RS3_W - 1 downto 0);
 
-  signal pc_pls_4_e       : std_logic_vector(C_ARCH_WORD_W - 1 downto 0);
-  signal rf_dout1_e       : std_logic_vector(C_ARCH_WORD_W - 1 downto 0);
-  signal rf_dout2_e       : std_logic_vector(C_ARCH_WORD_W - 1 downto 0);
-  signal rf_waddr_e       : std_logic_vector(C_ARCH_WORD_W - 1 downto 0);
-  signal imm_e            : std_logic_vector(C_ARCH_WORD_W - 1 downto 0);
-  signal dmem_dout_e      : std_logic_vector(C_ARCH_WORD_W - 1 downto 0);
+  signal pc_pls_4_e         : std_logic_vector(C_ARCH_WORD_W - 1 downto 0);
+  signal rf_dout1_e         : std_logic_vector(C_ARCH_WORD_W - 1 downto 0);
+  signal rf_dout2_e         : std_logic_vector(C_ARCH_WORD_W - 1 downto 0);
+  signal rf_waddr_e         : std_logic_vector(C_ARCH_WORD_W - 1 downto 0);
+  signal imm_e              : std_logic_vector(C_ARCH_WORD_W - 1 downto 0);
+  signal dmem_din_e         : std_logic_vector(C_ARCH_WORD_W - 1 downto 0);
 
-  signal alu_a_e          : std_logic_vector(C_ARCH_WORD_W - 1 downto 0);
-  signal alu_b_e          : std_logic_vector(C_ARCH_WORD_W - 1 downto 0);
-  signal alu_out_e        : std_logic_vector(C_ARCH_WORD_W - 1 downto 0);
+  signal alu_a_e            : std_logic_vector(C_ARCH_WORD_W - 1 downto 0);
+  signal alu_b_e            : std_logic_vector(C_ARCH_WORD_W - 1 downto 0);
+  signal alu_out_e          : std_logic_vector(C_ARCH_WORD_W - 1 downto 0);
+
+  signal is_0_e             : std_logic;
 
   -- Memory Stage Signals (_m)
-  signal alu_out_m        : std_logic_vector(C_ARCH_WORD_W - 1 downto 0);
-  signal dmem_dout_m      : std_logic_vector(C_ARCH_WORD_W - 1 downto 0);
-  signal rf_waddr_e       : std_logic_vector(C_ARCH_WORD_W - 1 downto 0);
+  signal alu_out_m          : std_logic_vector(C_ARCH_WORD_W - 1 downto 0);
+  signal dmem_din_m         : std_logic_vector(C_ARCH_WORD_W - 1 downto 0);
+  signal rf_waddr_m         : std_logic_vector(C_RF_ADDR_W - 1 downto 0);
 
   -- Write Back Stage Signals (_wb)
-  signal alu_out_m        : std_logic_vector(C_ARCH_WORD_W - 1 downto 0);
+  signal alu_out_wb         : std_logic_vector(C_ARCH_WORD_W - 1 downto 0);
+  signal dmem_dout_wb       : std_logic_vector(C_ARCH_WORD_W - 1 downto 0);
+  signal rf_waddr_wb        : std_logic_vector(C_RF_ADDR_W - 1 downto 0);
+  signal rf_din_wb          : std_logic_vector(C_ARCH_WORD_W - 1 downto 0);
 
 begin
+
   --*************************************************************************** FETCH STAGE
   --*********************************************************************************************
 
   ----------------------------------------------------------- ENTITY DEFINITION
+  U_PC_REG : entity work.reg_pipo("BEHAV_WITH_EN_INIT")
+    generic map (
+      DATA_W => pc_f'length, INIT_VAL => C_REG_INIT_VAL
+    )
+    port map (
+      CLK    => CLK,
+      RST_AN => RST_AN,
+      EN_N   => '0',
+      INIT   => '0',
+      DIN    => npc_f,
+      DOUT   => pc_f
+    );
+
+  U_IMEM : entity work.imem("Behavioural")
+    generic map (
+      ADDR_W => C_IMEM_ADDR_W,
+      DATA_W => C_ARCH_WORD_W
+    )
+    port map (
+      CLK    => CLK,
+      RST_AN => RST_AN,
+      RADDR  => pc_pls_4_f,
+      DOUT   => instr_f
+    );
 
   ----------------------------------------------------------- COMBINATORIAL
+  -- PC + 4 adder
+  pc_pls_4_f <= std_logic_vector(to_unsigned(to_integer(unsigned(npc_f)) + 4, pc_pls_4_f'length));
 
-  ----------------------------------------------------------- PROCESSES
+  -- NPC MUX select signal logic
+  npc_sel_f <= is_0_e when CTRL_WRD.branc_en = '1' else
+               '1' when CTRL_WRD.jump_en else
+               '0';
+
+  -- NPC MUX
+  npc_f <= alu_out_m when npc_sel_f = '1' else
+           pc_pls_4_f;
+
+  ----------------------------------------------------------- PIPELINE REGISTERS
+
+  U_PC_PLS_4_REG : entity work.reg_pipo("BEHAV_WITH_EN_INIT")
+    generic map (
+      DATA_W => pc_pls_4_f'length, INIT_VAL => C_REG_INIT_VAL
+    )
+    port map (
+      CLK    => CLK,
+      RST_AN => RST_AN,
+      EN_N   => '0',
+      INIT   => '0',
+      DIN    => pc_pls_4_f,
+      DOUT   => pc_pls_4_d
+    );
+
+  U_INSTR_REG : entity work.reg_pipo("BEHAV_WITH_EN_INIT")
+    generic map (
+      DATA_W => instr_f'length, INIT_VAL => C_REG_INIT_VAL
+    )
+    port map (
+      CLK    => CLK,
+      RST_AN => RST_AN,
+      EN_N   => '0',
+      INIT   => '0',
+      DIN    => instr_f,
+      DOUT   => instr_d
+    );
 
   --*************************************************************************** DECODE STAGE
   --*********************************************************************************************
 
   ----------------------------------------------------------- ENTITY DEFINITION
 
+  -- TODO put RF
+
+  U_RF_DOUT1_REG : entity work.reg_pipo("BEHAV_WITH_EN_INIT")
+    generic map (
+      DATA_W => rf_dout1_d'length, INIT_VAL => C_REG_INIT_VAL
+    )
+    port map (
+      CLK    => CLK,
+      RST_AN => RST_AN,
+      EN_N   => '0',
+      INIT   => '0',
+      DIN    => rf_dout1_d,
+      DOUT   => rf_dout1_e
+    );
+
   ----------------------------------------------------------- COMBINATORIAL
 
-  ----------------------------------------------------------- PROCESSES
+  rs1_d <= instr_d((C_INSTR_RS1_START_POS_BIT + C_INSTR_RS1_W) - 1 downto C_INSTR_RS1_START_POS_BIT);
+  rs2_d <= instr_d((C_INSTR_RS2_START_POS_BIT + C_INSTR_RS2_W) - 1 downto C_INSTR_RS2_START_POS_BIT);
+  rs3_d <= instr_d((C_INSTR_RS3_START_POS_BIT + C_INSTR_RS3_W) - 1 downto C_INSTR_RS3_START_POS_BIT);
+
+  imm_i_type_d <= instr_d((C_INSTR_I_TYPE_IMM_START_POS_BIT + C_INSTR_I_TYPE_IMM_W) - 1 downto C_INSTR_I_TYPE_IMM_START_POS_BIT);
+  imm_j_type_d <= instr_d((C_INSTR_J_TYPE_IMM_START_POS_BIT + C_INSTR_J_TYPE_IMM_W) - 1 downto C_INSTR_J_TYPE_IMM_W);
+
+  -- SIGN EXTEND on immediate for I-type instructions
+  imm_i_type_ext_d <= std_logic_vector(resize(signed(imm_i_type_d), C_ARCH_WORD_W)));
+
+  -- SIGN EXTEND on immediate for J-type instructions
+  imm_j_type_ext_d <= std_logic_vector(resize(signed(imm_j_type_d), C_ARCH_WORD_W)));
+
+  -- RF_WADDR MUX
+  rf_waddr_d <= std_logic_vector(to_unsigned(C_JAL_RET_ADDR_REG, rf_waddr_d'length)) when CTRL_WRD.jal_en = '1' else
+                rf_waddr_wb;
+
+  -- RF_DIN MUX
+  rf_din_d <= pc_pls_4_d when CTRL_WRD.jal_en = '1' else
+              rf_din_wb;
+
+  -- IMMEDIATE FROM JTYPE MUX
+  imm_d <= imm_j_type_ext_d when CTRL_WRD.j_type_imm_sel = '1' else
+           imm_i_type_ext_d;
+
+  ----------------------------------------------------------- PIPELINE REGISTERS
+  U_RF_DOUT2_REG : entity work.reg_pipo("BEHAV_WITH_EN_INIT")
+    generic map (
+      DATA_W => rf_dout2_d'length, INIT_VAL => C_REG_INIT_VAL
+    )
+    port map (
+      CLK    => CLK,
+      RST_AN => RST_AN,
+      EN_N   => '0',
+      INIT   => '0',
+      DIN    => rf_dout2_d,
+      DOUT   => rf_dout2_e
+    );
+
+  U_IMM_REG : entity work.reg_pipo("BEHAV_WITH_EN_INIT")
+    generic map (
+      DATA_W => imm_d'length, INIT_VAL => C_REG_INIT_VAL
+    )
+    port map (
+      CLK    => CLK,
+      RST_AN => RST_AN,
+      EN_N   => '0',
+      INIT   => '0',
+      DIN    => imm_d,
+      DOUT   => imm_e
+    );
+
+  U_RS2_REG : entity work.reg_pipo("BEHAV_WITH_EN_INIT")
+    generic map (
+      DATA_W => rs2_d'length, INIT_VAL => C_REG_INIT_VAL
+    )
+    port map (
+      CLK    => CLK,
+      RST_AN => RST_AN,
+      EN_N   => '0',
+      INIT   => '0',
+      DIN    => rs2_d,
+      DOUT   => rs2_e
+    );
+
+  U_RS3_REG : entity work.reg_pipo("BEHAV_WITH_EN_INIT")
+    generic map (
+      DATA_W => rs3_d'length, INIT_VAL => C_REG_INIT_VAL
+    )
+    port map (
+      CLK    => CLK,
+      RST_AN => RST_AN,
+      EN_N   => '0',
+      INIT   => '0',
+      DIN    => rs3_d,
+      DOUT   => rs3_e
+    );
 
   --*************************************************************************** EXECUTE STAGE
   --*****************************************************************************************
@@ -112,6 +291,29 @@ begin
       B    => alu_b_e,
       RES  => alu_out_e
     );
+
+  ----------------------------------------------------------- COMBINATORIAL
+
+  -- MUX ALU A
+  alu_a_e <= pc_pls_4_e when CTRL_WRD.pc_pls_4_sel = '1' else
+             rf_dout1_e;
+
+  -- MUX ALU B
+  alu_b_e <= imm_e when CTRL_WRD.imm_sel = '1' else
+             rf_dout2_e;
+
+  -- MUX RF WADDR
+  rf_waddr_e <= rs3_e when CTRL_WRD.r_type_sel = '1' else
+                rs2_e;
+
+  -- ZERO detector
+  is_0_e <= '1' when rf_dout1_e = (others => '0') else
+            '0';
+
+  -- Alias Signals for naming purposes
+  dmem_din_e <= rf_doutb_e;
+
+  ----------------------------------------------------------- PIPELINE REGISTERS
 
   -- TODO: rethink this design it gets busier the more registers we need maybe use a generate block
   U_REG_ALU_OUT_E : entity work.reg_pipo("BEHAV_WITH_EN_INIT")
@@ -129,15 +331,15 @@ begin
 
   U_REG_DMEM_DOUT_E : entity work.reg_pipo("BEHAV_WITH_EN_INIT")
     generic map (
-      DATA_W => dmem_dout_e'length, INIT_VAL => C_REG_INIT_VAL
+      DATA_W => dmem_din_e'length, INIT_VAL => C_REG_INIT_VAL
     )
     port map (
       CLK    => CLK,
       RST_AN => RST_AN,
       EN_N   => '0',
       INIT   => '0',
-      DIN    => dmem_dout_e,
-      DOUT   => dmem_dout_m
+      DIN    => dmem_din_e,
+      DOUT   => dmem_din_m
     );
 
   U_REG_RF_WADDR_E : entity work.reg_pipo("BEHAV_WITH_EN_INIT")
@@ -153,42 +355,74 @@ begin
       DOUT   => rf_waddr_m
     );
 
-  ----------------------------------------------------------- COMBINATORIAL
-
-  -- MUX ALU A
-  alu_a_e <= pc_pls_4_e when CTRL_WRD.pc_pls_4_sel = '1' else
-             rf_dout1_e;
-
-  -- MUX ALU B
-  alu_b_e <= imm_e when CTRL_WRD.imm_sel = '1' else
-             rf_dout2_e;
-
-  -- MUX RF WADDR
-  rf_waddr_e  <= rs3_e when CTRL_WRD.r_type_sel = '1' else
-                 rs2_e;
-
-  dmem_dout_e <= rf_doutb_e;
-
-  ----------------------------------------------------------- PROCESSES
-
   --*************************************************************************** MEMORY STAGE
   --*********************************************************************************************
 
   ----------------------------------------------------------- ENTITY DEFINITION
+  U_DMEM : entity work.dmem("Behavioural")
+    generic map (
+      ADDR_W => C_DMEM_ADDR_W,
+      DATA_W => C_ARCH_WORD_W
+    )
+    port map (
+      CLK    => CLK,
+      RST_AN => RST_AN,
+      RWADDR => alu_out_m,
+      WEN    => CTRL_WRD.DMEM_WE,
+      DIN    => dmem_din_m,
+      DOUT   => dmem_dout_m,
+    );
 
   ----------------------------------------------------------- COMBINATORIAL
 
-  ----------------------------------------------------------- PROCESSES
+  ----------------------------------------------------------- PIPELINE REGISTERS
+  U_REG_ALU_OUT_M : entity work.reg_pipo("BEHAV_WITH_EN_INIT")
+    generic map (
+      DATA_W => alu_out_m'length, INIT_VAL => C_REG_INIT_VAL
+    )
+    port map (
+      CLK    => CLK,
+      RST_AN => RST_AN,
+      EN_N   => '0',
+      INIT   => '0',
+      DIN    => alu_out_m,
+      DOUT   => alu_out_wb
+    );
 
-  
+  U_REG_DMEM_DOUT_M : entity work.reg_pipo("BEHAV_WITH_EN_INIT")
+    generic map (
+      DATA_W => dmem_dout_m'length, INIT_VAL => C_REG_INIT_VAL
+    )
+    port map (
+      CLK    => CLK,
+      RST_AN => RST_AN,
+      EN_N   => '0',
+      INIT   => '0',
+      DIN    => dmem_dout_m,
+      DOUT   => dmem_dout_wb
+    );
+
+  U_REG_RF_WADDR_M : entity work.reg_pipo("BEHAV_WITH_EN_INIT")
+    generic map (
+      DATA_W => rf_waddr_m'length, INIT_VAL => C_REG_INIT_VAL
+    )
+    port map (
+      CLK    => CLK,
+      RST_AN => RST_AN,
+      EN_N   => '0',
+      INIT   => '0',
+      DIN    => rf_waddr_m,
+      DOUT   => rf_waddr_wb
+    );
+
   --*************************************************************************** WRITE BACK STAGE
   --*********************************************************************************************
 
   ----------------------------------------------------------- ENTITY DEFINITION
 
   ----------------------------------------------------------- COMBINATORIAL
-
-  ----------------------------------------------------------- PROCESSES
+  rf_din_wb <= dmem_dout_wb when CTRL_WRD.rf_wb_dmem_dout_sel = '1' else
+               alu_dout_wb;
 
 end architecture BEHAVIORAL;
 
