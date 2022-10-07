d0f2c902e260a5c05eb7d284ea17f594afdc940a
Simone Ruffini <simone.ruffini@tutanota.com>
2022-10-07 23:53:30 +0200

    feat: updated assembler.sh to link IMEM init file

--- scripts/assembler/assembler.sh
+++ scripts/assembler/assembler.sh
@@ -3,7 +3,7 @@
 script_base_dir=$(cd $(dirname $0); pwd -P ) # Directory path of this script
 
 usage(){
-	echo "Usage: $(basename $0) <dlx_assembly_file>.asm"
+	echo "Usage: $(basename $0) <dlx_assembly_file>.asm [imem_init_symbolic_link.txt path]"
 }
 
 if [ -z "$1" ] || [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
@@ -16,6 +16,7 @@ if [ ! -r $1 ]; then
     usage
 	exit 1
 fi
+
 basename_asmfile=$(basename $1)  # get only file name without path
 asmfile="${basename_asmfile%.*}" # remove extension
 
@@ -24,3 +25,12 @@ ASSEMBLER_PATH="$script_base_dir/dlxasm.pl"
 perl $ASSEMBLER_PATH -o "$asmfile.bin" -list "$asmfile.list" "$1"
 rm "$asmfile.bin.hdr"
 cat "$asmfile.bin"| hexdump -v -e '/1 "%02X" /1 "%02X" /1 "%02X" /1 "%02X\n"' > "${asmfile}_dump.txt"
+
+if [ ! -L "$2" ]; then
+    ##echo "$2 not a symbolic link"
+    exit 0;
+fi
+
+## Link file to the asmfile_dump.txt
+echo "Linking '${asmfile}_dump.txt' to '$2' "
+ln -fs ${asmfile}_dump.txt "$2"
