e30e0b97e6179f2c18b3a58c1adaedf2e5c8555b
Bonnee <bonora.matteo@gmail.com>
2022-10-17 19:25:41 +0200

    Add test asm

---
+++ sim/asm_example/rtype_nop.asm
@@ -0,0 +1,30 @@
+seq r1,r3,r4
+nop
+nop
+nop
+nop
+sge r2,r5,r6
+nop
+nop
+nop
+nop
+sle r3,r1,r2
+nop
+nop
+nop
+nop
+slt r4,r1,r2
+nop
+nop
+nop
+nop
+add r5,r1,r2
+nop
+nop
+nop
+nop
+or  r6,r3,r5
+nop
+nop
+nop
+nop
