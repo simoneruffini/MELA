dd98125aeafddd1b0198b3cf61f6733db41d66ec
Bonnee <bonora.matteo@gmail.com>
2022-10-18 15:44:57 +0200

    Avoid data hazards in rtype_all.asm

--- sim/asm_example/rtype_all.asm
+++ sim/asm_example/rtype_all.asm
@@ -1,11 +1,27 @@
 sge r5,r1,r2
+nop
+nop
+nop
+nop
 add r1,r5,r5
+nop
+nop
+nop
+nop
 and r2,r1,r5
 or r1,r1,r5
+nop
+nop
+nop
+nop
 sle r3,r5,r1
 sll r4,r2,r2
 sne r8,r1,r2
 srl r7,r1,r2
 sub r9,r3,r1
-xor  r10,r9,r7
-sra  r11,r9,r7
+nop
+nop
+nop
+nop
+xor r10,r9,r7
+sra r11,r9,r7
