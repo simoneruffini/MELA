878b789461c352fa4de65aac7014ccbb3ddf2690
Bonnee <bonora.matteo@gmail.com>
2022-10-20 22:44:13 +0200

    Add itype instructions test

---
+++ sim/asm_example/itype.asm
@@ -0,0 +1,36 @@
+addi r1,r2,20
+nop
+nop
+nop
+andi r2,r1,170
+ori r3,r0,85
+nop
+nop
+sgei r4,r1,15
+slei r5 r3,32
+slli r6,r3,1
+nop
+snei r7,r4,1
+nop
+nop
+srli r8,r6,1
+b:
+subi r1,r1,2
+nop
+nop
+nop
+xori r10,r9,23
+nop
+nop
+beqz r0,a
+a:
+nop
+nop
+nop
+bnez r1,b
+nop
+nop
+nop
+nop
+nop
+nop
