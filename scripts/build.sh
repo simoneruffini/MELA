7a60bb320ed8e923a5a2bbb9181152773c4d113a
Simone Ruffini <simone.ruffini@tutanota.com>
2022-10-24 18:01:16 +0200

    fix: compilation and simulation save in sim folder

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,14 +1,41 @@
 #! /bin/env bash
 
 # Overridable variables
-VHDL_SRCS_LIST_FILE="./compile.f"
+VHDL_SRCS_LIST_FILE="./compile_sim.f"
 
 # Enter in the pwd of the script so the files that it uses work correctly
 base_dir=$(dirname $0)
 cd $base_dir
 
+# if first parameter is `syn` then do synthesis
+# then exit
+if  [ "$1" = "syn" ] ; then 
+
+    # Source this file to be able to run design compiler
+    source "/software/scripts/init_synopsys_64.11"
+
+    echo "#########################################"
+    echo "####                                 ####"
+    echo "##             SYNTHESIS               ##"
+    echo "####                                 ####"
+    echo "#########################################"
+    # go in syn folder
+    cd ../syn
+
+    # create work dir for library work
+    mkdir -p work
+
+    # design compiler shell with XG suport and tcl 
+    # https://www.edaboard.com/threads/whats-the-main-difference-between-dc_shell-dc_shell-t-dc_shell-xg-t.67023/
+    dc_shell-xg-t -no_log -f syn.do
+    exit $?
+fi 
+
 # Source this file to be able to run vsim,vcom,vlog etc...
-source /software/scripts/init_questa10.7c
+source "/software/scripts/init_questa10.7c"
+
+# outputs of comilation go into sim folder
+cd ../sim
 
 # delete work folder
 vdel -all
@@ -24,19 +51,25 @@ echo "#########################################"
 vcom -F $VHDL_SRCS_LIST_FILE -2008
 
 # check if verilog file compilation went ok otherwise exit
-if [ $? -ne 0 ]; then  exit; fi
+if [ $? -ne 0 ] ; then  exit; fi
 
 # run simulation if requested 
 
-# if first parameter is empty OR is not empty but not equal to all
+# if first parameter is empty exit normally 
+if [ -z "$1" ] ; then exit 0; fi
+
+# if first parapeter is 'sim` do simulation 
 # then exit
-if [ -z "$1" ] || ([ -n "$1" ] && [ "$1" != "all" ]) ; then exit; fi 
+if [ "$1" = "sim" ] ; then 
 
-echo "#########################################"
-echo "####                                 ####"
-echo "##             SIMULATION              ##"
-echo "####                                 ####"
-echo "#########################################"
+    echo "#########################################"
+    echo "####                                 ####"
+    echo "##             SIMULATION              ##"
+    echo "####                                 ####"
+    echo "#########################################"
 
-# Simulate the acc_tb entity using sim.do tcl script
-vsim -onfinish stop -c -sv_seed random -do sim.do tb_CPU
+    # Simulate the tb_CPU entity using sim.do tcl script
+    vsim -onfinish stop -c -sv_seed random -do sim.do tb_CPU
+    exit $?
+fi
+
--- sim/sim.do
+++ sim/sim.do
@@ -2,9 +2,9 @@ puts "\n########## SIMULATION STARTS ##########\n"
 run 212us
 puts "\n##########  SIMULATION ENDS  ##########\n"
 set dump_file [open "rf_dmem.dump" w]
-//set dump_file [open "rf_dmem.dump" a]
-//puts $dump_file ""
-//puts $dump_file [clock format [clock seconds] -format {%D %T}]
+#set dump_file [open "rf_dmem.dump" a]
+#puts $dump_file ""
+#puts $dump_file [clock format [clock seconds] -format {%D %T}]
 puts "REGISTER FILE MEMORY DUMP"
 set reg_val [examine sim:/tb_cpu/U_CPU/U_DATAPATH/U_RF/registers]
 puts $reg_val
@@ -16,5 +16,5 @@ set dmem_val [examine sim:/tb_cpu/U_DMEM/mem]
 puts $dmem_val
 puts $dump_file "DATA MEMORY DUMP"
 puts $dump_file $dmem_val
-//puts $dump_file ""
+#puts $dump_file ""
 exit
